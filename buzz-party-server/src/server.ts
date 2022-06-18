65242c4c2bcf4c984e6b52acad2d5815f0c8c97c
Tiago Ribeiro <tiago.err@outlook.com>
2022-06-18 01:16:03 +0100

    Updated the code to use a logger

--- buzz-party-server/src/server.ts
+++ buzz-party-server/src/server.ts
@@ -8,19 +8,19 @@ const log = SimpleNodeLogger.createSimpleLogger();
 const wss = generateWebSocket(parseInt(process.env.WSS_PORT || "8080"));
 
 wss.on("connection", (ws) => {
-	log.info("[WS] New client connected!");
+	log.info("[WSS] New client connected!");
 
 	ws.on("message", (data) => {
-		console.log(`[WSS] Client sent: ${data}`);
+		log.info(`[WSS] Client sent: ${data}`);
 	});
 
 	ws.on("close", () => {
-		console.log("[WSS] Client disconnected!");
+		log.info("[WSS] Client disconnected!");
 	});
 
 	ws.onerror = () => {
-		console.log("[WSS] Error occurred!");
+		log.info("[WSS] Error occurred!");
 	};
 });
 
-console.log(`[WSS] Started websocket server on port ${process.env.WSS_PORT || "8080"}!`);
+log.info(`[WSS] Started websocket server on port ${process.env.WSS_PORT || "8080"}!`);
