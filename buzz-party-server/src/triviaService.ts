235cf12a9565c671070af9505e69e893587bd199
Tiago Ribeiro <tiago.err@outlook.com>
2022-06-30 10:28:18 +0100

    Removed everything to restart

--- buzz-party-server/src/triviaService.ts
+++
@@ -1,8 +0,0 @@
-import {IOpenDBData} from "./interfaces";
-
-export async function retrieveSampleQuestions() {
-	const result = await fetch("https://opentdb.com/api.php?amount=10");
-	const data = (await result.json()) as IOpenDBData;
-
-	return data.results;
-}
